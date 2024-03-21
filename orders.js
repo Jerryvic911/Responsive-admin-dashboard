435c7f3b5e54d98c4e165dafc83738773c12a799
jeremiah victor <jerryvic791@gmail.com>
2024-03-21 22:14:18 +0100

    first commit

---
+++ orders.js
@@ -0,0 +1,52 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        shipping: 'pending'
+    },
+
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        PaymentStatus: 'DRefund',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        PaymentStatus: 'Due',
+        shipping: 'pending'
+    },
+
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        PaymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        PaymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        PaymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        PaymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+
+
+]
